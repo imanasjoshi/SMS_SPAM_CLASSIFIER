cb12b341dc5660b8651d74cc1593cedf18782cca
MANAS JOSHI <129400085+imanasjoshi@users.noreply.github.com>
2026-03-05 12:03:29 +0530

    Add NLTK downloads for tokenization and stopwords
    Added NLTK downloads for punkt and stopwords.

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
+# ADD THESE 3 LINES 👇
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 
@@ -40,14 +45,9 @@ st.title("Email/SMS Spam Classifier")
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-
-    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
